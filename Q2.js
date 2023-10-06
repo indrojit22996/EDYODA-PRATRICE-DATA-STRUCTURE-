c64fc9cf4e1ea83960c2b9a11e356e8ab9e811e1
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-10-06 19:27:57 +0530

    Reverse Array In Place

---
+++ Q2.js
@@ -0,0 +1,17 @@
+function reverseArrayInPlace(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    left++;
+    right--;
+  }
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+reverseArrayInPlace(originalArray);
+console.log("Reversed Array:", originalArray);
